1e305faf384012a71279651bee6924128fe2c7db
suresh lama <sureshlama@sureshs-MacBook-Air.local>
2025-01-30 21:15:50 +0545

    final commit

--- app/src/main/java/edu/suresh/passwordstrengthchecker/MainActivity.java
+++ app/src/main/java/edu/suresh/passwordstrengthchecker/MainActivity.java
@@ -9,6 +9,30 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    /*
+     * TODO: Check if the password is weak:
+     *       - Length is less than 4
+     *       - Does not contain a combination of capital and small letters, special characters, and numbers
+     *
+     * TODO: Check if the password is medium:
+     *       - Length is between 4 and 6 (exclusive)
+     *       - Contains both capital and small letters and numbers
+     *       - Does not include special characters
+     *
+     * TODO: Check if the password is strong:
+     *       - Length is more than 6
+     *       - Contains a combination of capital and small letters, special characters, and numbers
+     *
+     * TODO: Implement regex checks:
+     *       - To check if a word contains a capital letter: words.matches(".*[A-Z].*");
+     *       - To check if a word contains a small letter: words.matches(".*[a-z].*");
+     *       - To check if a word contains a number: words.matches(".*[0-9].*");
+     *       - To check if a word contains a special character: words.matches(".*[!@#$%^&*()\\-+={}\\[\\]|;:'\",.<>/?].*");
+     */
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
